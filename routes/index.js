854c52b3afd95bdc369e5e2def3c6571312c848a
Your Name <ushinigami97@gmail.com>
2020-08-22 20:26:13 +0200

    add serve static

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.setHeader('Cache-Control', 'public, max-age=86400');
+  res.setHeader('Cache-Control', 'public, max-age=31536000');
   res.render('index', { title: 'Student - Software Developer | ZeroDiverse' });
 });
 
