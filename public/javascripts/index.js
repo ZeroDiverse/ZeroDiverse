d07c3db4483d562625d9986afd1d479b19f4feeb
Your Name <ushinigami97@gmail.com>
2020-08-22 17:22:42 +0200

    basics mobile responsive

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,6 +4,7 @@ const btnClose = document.querySelector('.btn-close');
 btnOpen.addEventListener('click', () => {
     document.querySelector('.navbar-main .btn-open').style.display = 'none';
     document.querySelector('.navbar-main').style.width = '80%';
+    document.querySelector('main').style.opacity = '0.3';
     setTimeout(() => {
         document.querySelector('.navbar-main .btn-close').style.display = 'block';
     }, 200)
@@ -12,6 +13,7 @@ btnOpen.addEventListener('click', () => {
 btnClose.addEventListener('click', () => {
     document.querySelector('.navbar-main .btn-close').style.display = 'none';
     document.querySelector('.navbar-main').style.width = '0%';
+    document.querySelector('main').style.opacity = '1';
     setTimeout(() => {
         document.querySelector('.navbar-main .btn-open').style.display = 'block';
     }, 200)
