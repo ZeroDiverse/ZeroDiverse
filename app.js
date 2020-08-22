d32542362f8728426030c06a25a1722ccb67640c
Your Name <ushinigami97@gmail.com>
2020-08-22 12:23:31 +0200

    basics mobile project section

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const compression = require('compression');
 var app = express();
 
 // view engine setup
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(compression());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
