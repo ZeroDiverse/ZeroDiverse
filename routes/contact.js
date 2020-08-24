538fa78072e44d1cca4caba116d46ab68c33001f
Your Name <ushinigami97@gmail.com>
2020-08-24 20:01:21 +0200

    basics 14inch and 15inch screen

--- routes/contact.js
+++ routes/contact.js
@@ -10,4 +10,13 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/* GET users listing. */
+router.get('/collab', function(req, res, next) {
+  res.setHeader('Cache-Control', 'public, max-age=86400');
+  res.render('contact', {
+    title: 'Contact page - Say hello | ZeroDiverse',
+    path: 'contact'
+  });
+});
+
 module.exports = router;
