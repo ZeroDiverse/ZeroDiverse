323c2e55409ae811bf5c85da90012520da0a3e15
Your Name <ushinigami97@gmail.com>
2020-09-01 01:29:28 +0200

    fixed errors in routes

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/api/', require('./routes/api/index'));
-app.use('/api/contact', './routes/api/contact.js');
+app.use('/api/contact', require('./routes/api/contact.js'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
